9faa8cfc0e46c22c2cc825aa489ad8967e007a88
Chamodya111 <backchamodyapunsara@gmail.com>
2023-11-20 22:49:39 +0530

    second commit

---
+++ app/src/main/java/lk/ctag/ctagapp/Activity/DetailActivity.java
@@ -0,0 +1,16 @@
+package lk.ctag.ctagapp.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import lk.ctag.ctagapp.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
